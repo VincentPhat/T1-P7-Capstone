beac2fa663e810edaf88597deb72903867b9c212
VincentPhat <vincentphat02@gmail.com>
2022-04-11 10:27:22 +1200

    First Commit
    Commit of initial files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Project Capstone.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Project Capstone Contact Page.";
+
+            return View();
+        }
+    }
+}
